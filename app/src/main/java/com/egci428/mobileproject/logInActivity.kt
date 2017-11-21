c1d2bdaa61b110c9f6161068d305e3d282fec7c2
nekoabyss <neko.abyss@gmail.com>
2017-11-21 22:46:48 +0700

    ไก่กาก

---
+++ app/src/main/java/com/egci428/mobileproject/logInActivity.kt
@@ -0,0 +1,12 @@
+package com.egci428.mobileproject
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class logInActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+    }
+}
