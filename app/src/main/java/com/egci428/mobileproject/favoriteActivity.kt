9ad419dbbb91fa65bd379cfddaf9dcfebed40127
nekoabyss <neko.abyss@gmail.com>
2017-11-22 12:22:23 +0700

    input text -> hint

---
+++ app/src/main/java/com/egci428/mobileproject/favoriteActivity.kt
@@ -0,0 +1,12 @@
+package com.egci428.mobileproject
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class favoriteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorite)
+    }
+}
